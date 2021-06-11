712253902e20df932c3ddd85e1b96cd62825da1f
Guhyeon <ghgoo1798@gmail.com>
2021-06-11 09:31:35 +0900

    reload files

--- Book_this_is_coding_test/BOJ_coding_test/location_sorting_11650.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-n = int(input())
-locations = [list(map(int,sys.stdin.readline().rstrip().split())) for x in range(n)]
-
-locations.sort(key = lambda location : (location[0], location[1]))
-
-for x in locations:
-    print(x[0], x[1])
