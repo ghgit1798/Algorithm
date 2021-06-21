185de728056445077cf0524698693db718a33a0e
Guhyeon <ghgoo1798@gmail.com>
2021-06-21 09:33:16 +0900

    스택큐 - 기능개발 best solution 추가

--- Programmers/스택큐_기능개발.py
+++ Programmers/스택큐_기능개발.py
@@ -34,7 +34,15 @@ def solution(progresses, speeds):
             progresses[i] += speed
         
     return answer
-    
+
+def best_solution(progresses, speeds):
+    Q=[]
+    for p, s in zip(progresses, speeds):
+        if len(Q)==0 or Q[-1][0]<-((p-100)//s):
+            Q.append([-((p-100)//s),1])
+        else:
+            Q[-1][1]+=1
+    return [q[1] for q in Q]
 
 progresses = [93, 30, 55]
 speeds = [1, 30, 5]
